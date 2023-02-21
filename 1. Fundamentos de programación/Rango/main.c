e2451429edcb2c08f187dc7845329d8713d45103
FranCisco216203 <yatsumatsu@gmail.com>
2023-02-21 15:00:42 -0600

    practica 2

---
+++ 1. Fundamentos de programación/Rango/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{ printf("buscar numero en el rango\n");
+    int x=1;
+    int inicio= 1;
+    int final= 10;
+    printf("el rango es %d a %d \n",inicio,final);
+    if(x>inicio && x<final){
+        printf("el numero %d esta dentro del rango %d - %d\n",x,inicio,final);
+    } else{
+   printf("el numero %d esta fuera del rango %d - %d\n",x,inicio,final); }
+}
---
+++ 1. Fundamentos de programación/codeblocks/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("evaluacion de numeros \n");
+    int numero1= 10;
+    int numero2= 50;
+    if(numero1>numero2){
+        printf("%d es mayor %d",numero1,numero2);
+    } else{
+    printf("%d es menor %d",numero1,numero2);
+    }
+    return 0;
+}
