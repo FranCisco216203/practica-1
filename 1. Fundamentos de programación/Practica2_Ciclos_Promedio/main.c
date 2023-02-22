bfaa6f0043afc11fcde68e11bcc19cef73b063a3
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2023-02-22 15:37:53 -0600

    Update main.c

--- 1. Fundamentos de programación/Practica2_Ciclos_Promedio/main.c
+++ 1. Fundamentos de programación/Practica2_Ciclos_Promedio/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+    Ejemplos
+     1+2+3 = 2
+    ALGORITMO PARA CALCULAR PROMEDIO DE 3 NÚMEROS
+    0. EL programa muestra un mensaje que dice: "PROMEDIO DE 3 NUMEROS"
+    1. EL programa crea una variable para el primero número
+    2. EL programa crea una variable para el segundo número
+    3. EL programa crea una variable para el tercer número
+    4. El programa inicializa el primer numero
+    5. El programa inicializa el segundo numero
+    6. El programa inicializa el tercer numero
+    7. El programa suma las 3 variables
+    8. EL programa divide entre 3 el resultado del paso 7
+    9. El programa muestra el resultado al usuario
 
+
+
+*/
 int main()
 {
-    printf("Hello world!\n");
+    //printf("Hello world!\n");
     return 0;
 }
