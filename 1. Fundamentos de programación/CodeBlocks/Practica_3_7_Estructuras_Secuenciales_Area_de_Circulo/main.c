384be5077ec9c2cb11f50c8301dbdf904ce84e94
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2023-03-03 14:02:24 -0600

    Avace del circulo

--- 1. Fundamentos de programación/CodeBlocks/Practica_3_7_Estructuras_Secuenciales_Area_de_Circulo/main.c
+++ 1. Fundamentos de programación/CodeBlocks/Practica_3_7_Estructuras_Secuenciales_Area_de_Circulo/main.c
@@ -1,6 +1,6 @@
 /* INFORMACION
 *
-* MATERIA:
+* MATERIA: Fundamentos...
 * PROGRAMADOR:
 * CONTACTO:
 * FECHA:
@@ -10,7 +10,7 @@
 */
 
 /* ALGORITMO
-    1. El programa muestra el titulo "".
+    1. El programa muestra el titulo "Calculo del area del circulo".
     2.
     3.
     4.
@@ -20,9 +20,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int pi_int = 3;
+    float pi_float = 3.1416;
+
+    printf("%d, %f", pi_int, pi_float);
+
     return 0;
 }
