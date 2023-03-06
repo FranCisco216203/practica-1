4da9895bbb8fb8d8cd01534041eaefab8139da6e
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2023-03-06 17:19:20 -0600

    Organizado...

--- 1. Fundamentos de programación/CodeBlocks/Practica_2_3_Diagrama_de_Flujo_Pares_Impares/main.c
+++ 1. Fundamentos de programación/CodeBlocks/Practica_2_3_Diagrama_de_Flujo_Pares_Impares/main.c
@@ -21,8 +21,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+void caratula(){
+    int numero=23;
+    printf("*****************************************************\n");
+    printf("* hola mi nombre es francisco tengo %d años de edad *\n",numero );
+    printf("* estudio en la UAM de iztapalapa                   *\n");
+    printf("*                     PRACTICA 1                    *\n");
+    printf("*****************************************************\n\n");
+}
+void muestraPI(){
+    float pi= 23.141516;
+    printf("el valor de pi es igual a %f \n",pi);
+}
+
+int suma (int A,int B){
+    return A+B;
+}
+
+
+int main(){
+    printf("buscar numero en el rango\n");
+    int x=1;
+    int inicio= 1;
+    int final= 10;
+    printf("el rango es %d a %d \n",inicio,final);
+    if(x>inicio && x<final){
+        printf("el numero %d esta dentro del rango %d - %d\n",x,inicio,final);
+    } else{
+   printf("el numero %d esta fuera del rango %d - %d\n",x,inicio,final); }
 }
