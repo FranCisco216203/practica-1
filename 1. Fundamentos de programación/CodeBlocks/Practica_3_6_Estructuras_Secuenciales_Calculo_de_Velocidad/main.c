7b5374faa8bb90b2a3830add9cf4b292098df1e7
FranCisco216203 <yatsumatsu@gmail.com>
2023-03-14 01:00:05 -0600

    practica_velocidad

--- 1. Fundamentos de programación/CodeBlocks/Practica_3_6_Estructuras_Secuenciales_Calculo_de_Velocidad/main.c
+++ 1. Fundamentos de programación/CodeBlocks/Practica_3_6_Estructuras_Secuenciales_Calculo_de_Velocidad/main.c
@@ -1,21 +1,23 @@
 /* INFORMACION
 *
 * MATERIA:
-* PROGRAMADOR:
+* PROGRAMADOR: FRANCISCO ANTONIO GONZALEZ NOLASCO
 * CONTACTO:
-* FECHA:
+* FECHA:14/03/2023
 *
 * COMPILAR:
 * EJECUTAR:
 */
 
 /* ALGORITMO
-    1. El programa muestra el titulo "".
-    2.
-    3.
-    4.
-    5. El programa termina.
-
+    1. El programa muestra el titulo "CALCULO DE VELOCIDAD".
+    2. EL programa crea una variable para el primero número
+    3. EL programa crea una variable para el segundo número
+    4. El programa inicializa el primer numero
+    5. El programa inicializa el segundo numero
+    6. el programa crea una tercera variable apartir de las 2 primeras
+    7. el programa crea una variable para guardar el resultado
+    8. El programa muestra el resultado al usuario
 */
 
 #include <stdio.h>
@@ -23,6 +25,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    /*printf("CALCULO DE VELOCIDAD\n");
+    float distancia= 10;
+    float tiempo= 30;
+    printf("sea la distancia %.1f y el tiempo %.1f \n",distancia,tiempo);
+    float velocidad =distancia/tiempo;
+    printf("la verlocidad sera igual a %.3f \n", velocidad);*/
+
+    printf("CALCULO DE VELOCIDAD\n");
+    int distancia;
+    int tiempo;
+    printf("sea una distancia = \n");
+    scanf("%f",distancia);
+    printf("y sea un tiempo = \n");
+    scanf("%f",tiempo);
+    float velocidad =distancia/tiempo;
+    printf("la verlocidad sera igual a %.3f \n", velocidad);
+
     return 0;
 }
